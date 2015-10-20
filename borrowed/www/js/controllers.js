8d9d8c54570b57136ee1bf3f5395fb4134d8bcc7
Allie Wilson <aswilson@andrew.cmu.edu>
2015-10-20 01:48:52 -0400

    functionality for moving from nudge page when a shake occurs is there and not throwing eny errors, but can't yet tell if it's actually working

--- borrowed/www/js/controllers.js
+++ borrowed/www/js/controllers.js
@@ -120,20 +120,98 @@ angular.module('starter.controllers', [])
 	$rootScope.showFooter = true;
 })
 
-.controller('NudgeCtrl', function($scope, $stateParams, $rootScope) {
+.controller('NudgeCtrl', function($scope, $stateParams, $rootScope, $ionicPlatform, $cordovaDeviceMotion) {
+	$scope.options = { 
+	        frequency: 100, // Measure every 100ms
+	        deviation : 15  // We'll use deviation to determine the shake event, best values in the range between 25 and 30
+	    };
+		$scope.measurements = {
+		        x : null,
+		        y : null,
+		        z : null,
+		        timestamp : null
+		}
+				$scope.previousMeasurements = {
+			        x : null,
+			        y : null,
+			        z : null,
+			        timestamp : null
+			    } 
+				$scope.watch = null;
+				$ionicPlatform.ready(function() {
+ 
+				        //Start Watching method
+				        $scope.startWatching = function() {     
+ 
+				            // Device motion configuration
+				            $scope.watch = $cordovaDeviceMotion.watchAcceleration($scope.options);
+ 
+				            // Device motion initilaization
+				            $scope.watch.then(null, function(error) {
+				                console.log('Error');
+				            },function(result) {
+ 
+				                // Set current data  
+				                $scope.measurements.x = result.x;
+				                $scope.measurements.y = result.y;
+				                $scope.measurements.z = result.z;
+				                $scope.measurements.timestamp = result.timestamp;                 
+ 
+				                // Detecta shake  
+				                $scope.detectShake(result);  
+ 
+				            });     
+				        };      
+ 
+				        // Stop watching method
+				        $scope.stopWatching = function() {  
+				            $scope.watch.clearWatch();
+				        }       
+ 
+				        // Detect shake method      
+				        $scope.detectShake = function(result) { 
+ 
+				            //Object to hold measurement difference between current and old data
+				            var measurementsChange = {};
+ 
+				            // Calculate measurement change only if we have two sets of data, current and old
+				            if ($scope.previousMeasurements.x !== null) {
+				                measurementsChange.x = Math.abs($scope.previousMeasurements.x, result.x);
+				                measurementsChange.y = Math.abs($scope.previousMeasurements.y, result.y);
+				                measurementsChange.z = Math.abs($scope.previousMeasurements.z, result.z);
+				            }
+ 
+				            // If measurement change is bigger then predefined deviation
+				            if (measurementsChange.x + measurementsChange.y + measurementsChange.z > $scope.options.deviation) {
+				                $scope.stopWatching();  // Stop watching because it will start triggering like hell
+				                console.log('Shake detected'); // shake detected
+				  
+ 
+				                // Clean previous measurements after succesfull shake detection, so we can do it next time
+				                $scope.previousMeasurements = { 
+				                    x: null, 
+				                    y: null, 
+				                    z: null
+				                }               
+								$state.go('tab.chats');
+				            } else {
+				                // On first measurements set it as the previous one
+				                $scope.previousMeasurements = {
+				                    x: result.x,
+				                    y: result.y,
+				                    z: result.z
+				                }
+				            }           
+ 
+				        }       
+ 
+				    });
+ 
+				    $scope.$on('$ionicView.beforeLeave', function(){
+				        $scope.watch.clearWatch(); // Turn off motion detection watcher
+				    }); 
+					
 	
-	$scope.goBack = function(){
-		$state.go("tab.chats")
-	}
-		
-	ionic.Platform.ready(function(){
-		console.log("deviceready");
-		shake.startWatch(
-			function(){
-				console.log("device is being shaken");
-				$scope.goBack();
-				$scope.$apply();
-			}, 40);
-	})
+	
 	$rootScope.showFooter = true;
 });
